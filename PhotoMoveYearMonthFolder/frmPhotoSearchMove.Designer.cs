a7fd0259ea3bf773043fb9459731ac0f867f8da0
Firefox_1998 ... <firefox_1998@hotmail.com>
2024-02-25 08:34:35 +0100

    Prima release

---
+++ PhotoMoveYearMonthFolder/frmPhotoSearchMove.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PhotoMoveYearMonthFolder
+{
+    partial class FrmPhotoSearchMove
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Lbl_DirSearch = new Label();
+            Btn_DirSearch = new Button();
+            Lbl_DirDestination = new Label();
+            Btn_DirDest = new Button();
+            Btn_Start = new Button();
+            Fbd_DirSel = new FolderBrowserDialog();
+            SuspendLayout();
+            // 
+            // Lbl_DirSearch
+            // 
+            Lbl_DirSearch.BorderStyle = BorderStyle.FixedSingle;
+            Lbl_DirSearch.Location = new Point(12, 24);
+            Lbl_DirSearch.Name = "Lbl_DirSearch";
+            Lbl_DirSearch.Size = new Size(327, 18);
+            Lbl_DirSearch.TabIndex = 0;
+            Lbl_DirSearch.Text = "Directory Search";
+            // 
+            // Btn_DirSearch
+            // 
+            Btn_DirSearch.Location = new Point(360, 12);
+            Btn_DirSearch.Name = "Btn_DirSearch";
+            Btn_DirSearch.Size = new Size(102, 44);
+            Btn_DirSearch.TabIndex = 1;
+            Btn_DirSearch.Text = "Select Directory To Image Search";
+            Btn_DirSearch.UseVisualStyleBackColor = true;
+            Btn_DirSearch.Click += Btn_DirSearch_Click;
+            // 
+            // Lbl_DirDestination
+            // 
+            Lbl_DirDestination.BorderStyle = BorderStyle.FixedSingle;
+            Lbl_DirDestination.Location = new Point(12, 73);
+            Lbl_DirDestination.Name = "Lbl_DirDestination";
+            Lbl_DirDestination.Size = new Size(327, 18);
+            Lbl_DirDestination.TabIndex = 2;
+            Lbl_DirDestination.Text = "Directory Destination";
+            // 
+            // Btn_DirDest
+            // 
+            Btn_DirDest.Location = new Point(360, 59);
+            Btn_DirDest.Name = "Btn_DirDest";
+            Btn_DirDest.Size = new Size(102, 44);
+            Btn_DirDest.TabIndex = 3;
+            Btn_DirDest.Text = "Select Directory To Image Copy";
+            Btn_DirDest.UseVisualStyleBackColor = true;
+            Btn_DirDest.Click += Btn_DirDest_Click;
+            // 
+            // Btn_Start
+            // 
+            Btn_Start.Location = new Point(360, 705);
+            Btn_Start.Name = "Btn_Start";
+            Btn_Start.Size = new Size(102, 44);
+            Btn_Start.TabIndex = 4;
+            Btn_Start.Text = "Start";
+            Btn_Start.UseVisualStyleBackColor = true;
+            Btn_Start.Click += Btn_Start_Click;
+            // 
+            // Fbd_DirSel
+            // 
+            Fbd_DirSel.SelectedPath = "C:\\";
+            // 
+            // FrmPhotoSearchMove
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(474, 761);
+            Controls.Add(Btn_Start);
+            Controls.Add(Btn_DirDest);
+            Controls.Add(Lbl_DirDestination);
+            Controls.Add(Btn_DirSearch);
+            Controls.Add(Lbl_DirSearch);
+            Name = "FrmPhotoSearchMove";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Photo Search, Move And Divide to Year - Month";
+            FormClosing += FrmPhotoSearchMove_FormClosing;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label Lbl_DirSearch;
+        private Button Btn_DirSearch;
+        private Label Lbl_DirDestination;
+        private Button Btn_DirDest;
+        private Button Btn_Start;
+        private FolderBrowserDialog Fbd_DirSel;
+    }
+}
---
+++ PhotoMoveYearMonthFolder/frmPhotoSearchMove.cs
@@ -0,0 +1,228 @@
+using System.Collections.Concurrent;
+using System.Globalization;
+using System.Threading;
+
+namespace PhotoMoveYearMonthFolder
+{
+    public partial class FrmPhotoSearchMove : Form
+    {
+        private SemaphoreSlim semaphoreLock = new SemaphoreSlim(1, 1);
+
+
+        private Label[] Lbl_Desc = new Label[25]; // Crea un array di n Label
+        private Label[] Lbl_FileNameProc = new Label[25]; // Crea un array di n Label
+
+        private string sSearchDir = "";
+        private string sDestDir = "";
+        private readonly object fileLock = new();
+        private bool isProcessing = false;
+
+        public FrmPhotoSearchMove()
+        {
+            InitializeComponent();
+            for (int i = 0; i < Lbl_Desc.Length; i++)
+            {
+                Lbl_Desc[i] = new Label
+                {
+                    Name = "Lbl_Desc" + i,
+                    AutoSize = true,
+                    Text = "File processed thread " + (i + 1).ToString("D2") + ":",
+                    Location = new Point(12, (i * 20) + 127) // Posiziona le label verticalmente
+                };
+                this.Controls.Add(Lbl_Desc[i]); // Aggiungi la label alla form
+
+                Lbl_FileNameProc[i] = new Label
+                {
+                    Name = "Lbl_FileNameProc" + i,
+                    AutoSize = true,
+                    Text = "-",
+                    Location = new Point(150, (i * 20) + 127) // Posiziona le label verticalmente
+                };
+                this.Controls.Add(Lbl_FileNameProc[i]); // Aggiungi la label alla form
+            }            
+        }
+
+        private async void Btn_Start_Click(object sender, EventArgs e)
+        {
+            isProcessing = true;
+            if (!string.IsNullOrEmpty(sSearchDir) && !string.IsNullOrEmpty(sDestDir) && !sSearchDir.Equals(sDestDir))
+            {
+                Btn_DirDest.Enabled = false;
+                Btn_DirSearch.Enabled = false;
+                Btn_Start.Enabled = false;
+
+                // Elabora tutte le immagini nella cartella e nelle sottocartelle
+                try
+                {
+                    var semaphore = new SemaphoreSlim(25); // Imposta il numero massimo di thread a 25
+                    var files = Directory.EnumerateFiles(sSearchDir, "*", SearchOption.AllDirectories);
+                    var tasks = new List<Task>();
+
+                    int i = 0;
+                    foreach (var file in files)
+                    {                        
+                        // Controlla se è un file immagine
+                        if (IsValidFile(file))
+                        {
+                            var label = Lbl_FileNameProc[i % Lbl_FileNameProc.Length];
+                            tasks.Add(Task.Run(async () =>
+                            {
+                                await semaphore.WaitAsync();
+                                try
+                                {
+                                    await ProcessFileAsync(file, label);
+                                }
+                                finally
+                                {
+                                    semaphore.Release();
+                                }
+                            }));
+                            i++;
+                        }
+                    }
+                    await Task.WhenAll(tasks);
+                    MessageBox.Show("Elaborazione completata!", "Informazioni", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Si è verificato un errore: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                Btn_DirDest.Enabled = true;
+                Btn_DirSearch.Enabled = true;
+                Btn_Start.Enabled = true;
+            }
+            isProcessing = false;
+        }
+
+        private void Btn_DirSearch_Click(object sender, EventArgs e)
+        {
+            if (Fbd_DirSel.ShowDialog() == DialogResult.OK)
+            {
+                sSearchDir = Fbd_DirSel.SelectedPath;
+                Lbl_DirSearch.Text = sSearchDir;
+            }
+        }
+
+        private void Btn_DirDest_Click(object sender, EventArgs e)
+        {
+            if (Fbd_DirSel.ShowDialog() == DialogResult.OK)
+            {
+                sDestDir = Fbd_DirSel.SelectedPath;
+                Lbl_DirDestination.Text = sDestDir;
+            }
+        }
+
+        private static bool IsValidFile(string file)
+        {
+            // Ottieni l'estensione del file
+            string extension = Path.GetExtension(file).ToLower();
+
+            // Elenco di estensioni di file immagine
+            string[] validExtensions = [".jpg", ".jpeg", ".png", ".bmp", ".gif", ".mp4"];
+
+            // Restituisce true se l'estensione è presente nell'elenco
+            return validExtensions.Contains(extension);
+        }
+
+        private static string GenerateNewFileName(string filePath)
+        {
+            string directory = Path.GetDirectoryName(filePath) ?? throw new ArgumentNullException(nameof(filePath));
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+            string fileExtension = Path.GetExtension(filePath);
+
+            int counter = 1;
+            string newFileName = $"{fileNameWithoutExtension}_{counter}{fileExtension}";
+
+            while (File.Exists(Path.Combine(directory, newFileName)))
+            {
+                counter++;
+                newFileName = $"{fileNameWithoutExtension}_{counter}{fileExtension}";
+            }
+
+            return Path.Combine(directory, newFileName);
+        }
+
+        private async Task ProcessFileAsync(string file, Label label)
+        {
+
+            // Acquisisci il semaforo (equivalente a entrare in un blocco 'lock')
+            await semaphoreLock.WaitAsync();
+            try
+            {
+                if (file.Length > 30)
+                {
+                    label.Invoke((Action)(() => label.Text = file.Substring(file.Length - 30, 30)));
+                }
+                else
+                {
+                    label.Invoke((Action)(() => label.Text = file));
+                }
+
+                // Ottieni i primi sei caratteri del nome file
+                string nomeFile = Path.GetFileNameWithoutExtension(file);
+                string anno = nomeFile[..4];
+                string mese = nomeFile.Substring(4, 2);
+
+                if (anno.Equals("IMG-", StringComparison.OrdinalIgnoreCase) || anno.Equals("VID-", StringComparison.OrdinalIgnoreCase))
+                {
+                    anno = nomeFile.Substring(4, 4);
+                    mese = nomeFile.Substring(8, 2);
+                }
+
+                if (DateTime.TryParseExact(anno + mese,
+                                            "yyyyMM",
+                                            provider: CultureInfo.InvariantCulture,
+                                            DateTimeStyles.None,
+                                            out _))
+                {
+                    // Crea la cartella anno se non esiste
+                    string cartellaAnno = Path.Combine(sDestDir, anno);
+                    if (!Directory.Exists(cartellaAnno))
+                    {
+                        Directory.CreateDirectory(cartellaAnno);
+                    }
+
+                    // Crea la cartella mese se non esiste
+                    string cartellaMese = Path.Combine(cartellaAnno, mese);
+                    if (!Directory.Exists(cartellaMese))
+                    {
+                        Directory.CreateDirectory(cartellaMese);
+                    }
+
+                    // Copia il file nella cartella mese
+                    string destinazioneFile = Path.Combine(cartellaMese, nomeFile + Path.GetExtension(file));
+                    if (File.Exists(destinazioneFile))
+                    {
+                        string newFileName = GenerateNewFileName(destinazioneFile);
+                        await CopyFileAsync(file, newFileName);
+                    }
+                    else
+                    {
+                        await CopyFileAsync(file, destinazioneFile);
+                    }
+                }
+            }
+            finally
+            {
+                // Rilascia il semaforo (equivalente a uscire da un blocco 'lock')
+                semaphoreLock.Release();
+            }            
+        }
+
+        private void FrmPhotoSearchMove_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isProcessing)
+            {
+                e.Cancel = true;
+                MessageBox.Show("Non è possibile chiudere la form durante l'elaborazione.", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private async Task CopyFileAsync(string sourceFile, string destinationFile)
+        {
+            using FileStream sourceStream = new(sourceFile, FileMode.Open, FileAccess.Read);
+            using var destinationStream = new FileStream(destinationFile, FileMode.Create, FileAccess.Write);
+            await sourceStream.CopyToAsync(destinationStream);
+        }
+    }
+}
